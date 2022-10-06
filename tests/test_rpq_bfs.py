ffd573e163103ea3456a93096d9f431fd1873f4e
Sergey Loktev <loktevsergay.2003@gmail.com>
2022-10-06 19:02:03 +0300

    Added tests for rpq_bfs function

--- tests/test_rpq_bfs.py
+++ tests/test_rpq_bfs.py
@@ -3,8 +3,20 @@ import networkx as nx
 from project import build_two_cycle_labeled_graph, rpq_bfs
 
 
-def make_graph():
+def make_graph_two_cycled():
     return build_two_cycle_labeled_graph(5, 3, ("A", "B"))
+
+def make_graph():
+    graph = nx.MultiDiGraph()
+    graph.add_edge(0, 1, label="A")
+    graph.add_edge(0, 2, label="B")
+    graph.add_edge(1, 3, label="C")
+    graph.add_edge(1, 3, label="D")
+    graph.add_edge(2, 3, label="C")
+    graph.add_edge(2, 3, label="D")
+    graph.add_edge(3, 4, label="E")
+    graph.add_edge(4, 5, label="E")
+    return graph
 
 
 def test_rpq_bfs_empty_graph():
@@ -14,7 +26,7 @@ def test_rpq_bfs_empty_graph():
 
 
 def test_rpq_bfs_single_start_single_final():
-    graph = make_graph()
+    graph = make_graph_two_cycled()
     expected_rpq = {0: {6}}
     actual_rpq = rpq_bfs(
         graph=graph,
@@ -27,10 +39,38 @@ def test_rpq_bfs_single_start_single_final():
 
 
 def test_rpq_bfs_all_start_and_final():
-    graph = make_graph()
+    graph = make_graph_two_cycled()
     expected_rpq = {0, 6, 7, 8}
     actual_rpq = rpq_bfs(
         graph=graph,
         query="AAAAAA|B",
     )
     assert actual_rpq == expected_rpq
+
+def test_rpq_bfs_1():
+    graph = make_graph()
+    expected_rpq = {3}
+    actual_rpq = rpq_bfs(
+        graph=graph,
+        query="(A|B)C(D*)(E*)",
+        start_nodes={0},
+        final_nodes={3}
+    )
+    assert actual_rpq == expected_rpq
+
+
+def test_rpq_bfs_2():
+    graph = make_graph()
+    expected_rpq = {0}
+    actual_rpq = rpq_bfs(
+        graph=graph,
+        query="(A|C)DEE",
+        start_nodes={0},
+        final_nodes={5},
+        all_reachable=True
+    )
+    assert actual_rpq == expected_rpq
+
+test_rpq_bfs_2()
+
+
