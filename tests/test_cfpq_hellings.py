4c94ad154189440cc07989ddb7f158c08000f27b
Sergey Loktev <71882967+zishkaz@users.noreply.github.com>
2022-11-23 21:38:23 +0300

    Matrix CFPQ implemented

--- tests/test_cfpq_hellings.py
+++ tests/test_cfpq_hellings.py
@@ -1,28 +1,28 @@
 from pyformlang.cfg import CFG
 
-from project import cfpq, build_two_cycle_labeled_graph
+from project import run_cfpq, build_two_cycle_labeled_graph
 
 
-def test_cfpq_1():
+def test_cfpq_hellings_1():
     cfg_as_text = """
             S -> epsilon
             """
     graph = build_two_cycle_labeled_graph(1, 1, ("A", "B"))
     reachable_pairs = {(0, 0), (1, 1), (2, 2)}
-    assert cfpq(graph=graph, cfg=CFG.from_text(cfg_as_text)) == reachable_pairs
+    assert run_cfpq(algo="hellings", graph=graph, cfg=CFG.from_text(cfg_as_text)) == reachable_pairs
 
 
-def test_cfpq_2():
+def test_cfpq_hellings_2():
     cfg_as_text = """
             S -> a S
             S -> epsilon
             """
     graph = build_two_cycle_labeled_graph(1, 1, ("a", "b"))
     reachable_pairs = {(0, 1), (0, 0), (1, 1), (2, 2), (1, 0)}
-    assert cfpq(graph=graph, cfg=CFG.from_text(cfg_as_text)) == reachable_pairs
+    assert run_cfpq(algo="hellings", graph=graph, cfg=CFG.from_text(cfg_as_text)) == reachable_pairs
 
 
-def test_cfpq_3():
+def test_cfpq_hellings_3():
     cfg_as_text = """
             S -> ( S ) S
             S -> S ( S )
@@ -31,9 +31,10 @@ def test_cfpq_3():
     graph = build_two_cycle_labeled_graph(2, 3, ("a", "b"))
     reachable_pairs = {(4, 4), (5, 5), (0, 0), (1, 1), (3, 3), (2, 2)}
     assert (
-        cfpq(
-            graph=graph,
-            cfg=CFG.from_text(cfg_as_text),
-        )
-        == reachable_pairs
+            run_cfpq(
+                algo="hellings",
+                graph=graph,
+                cfg=CFG.from_text(cfg_as_text),
+            )
+            == reachable_pairs
     )
---
+++ tests/test_cfpq_matrix.py
@@ -0,0 +1,40 @@
+from pyformlang.cfg import CFG
+
+from project import build_two_cycle_labeled_graph, run_cfpq
+
+
+def test_cfpq_matrix_1():
+    cfg_as_text = """
+            S -> epsilon
+            """
+    graph = build_two_cycle_labeled_graph(1, 1, ("A", "B"))
+    reachable_pairs = {(0, 0), (1, 1), (2, 2)}
+    assert run_cfpq(algo="matrix", graph=graph, cfg=CFG.from_text(cfg_as_text)) == reachable_pairs
+
+
+def test_cfpq_matrix_2():
+    cfg_as_text = """
+            S -> a S
+            S -> epsilon
+            """
+    graph = build_two_cycle_labeled_graph(1, 1, ("a", "b"))
+    reachable_pairs = {(0, 1), (0, 0), (1, 1), (2, 2), (1, 0)}
+    assert run_cfpq(algo="matrix", graph=graph, cfg=CFG.from_text(cfg_as_text)) == reachable_pairs
+
+
+def test_cfpq_matrix_3():
+    cfg_as_text = """
+            S -> ( S ) S
+            S -> S ( S )
+            S -> epsilon
+            """
+    graph = build_two_cycle_labeled_graph(2, 3, ("a", "b"))
+    reachable_pairs = {(4, 4), (5, 5), (0, 0), (1, 1), (3, 3), (2, 2)}
+    assert (
+            run_cfpq(
+                algo="matrix",
+                graph=graph,
+                cfg=CFG.from_text(cfg_as_text),
+            )
+            == reachable_pairs
+    )
